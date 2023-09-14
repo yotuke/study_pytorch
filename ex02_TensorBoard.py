65979636db3f819f009c4e970ee692e977d741ed
study_py <yotuke@outlook.com>
2023-09-14 15:54:55 +0800

    tensorboard结束

--- ex02_TensorBoard.py
+++ ex02_TensorBoard.py
@@ -1,12 +1,34 @@
 """
     TensorBoard 的使用
 """
-# from tensorboardX import SummaryWriter
 from tensorboardX import SummaryWriter
-if __name__ == "__main__":
-    writer = SummaryWriter("test_log")
-    for i in range(100):
-        writer.add_scalar("y=x^2", i*i, i)
+from PIL import Image
+import numpy
+
+IMAGE_URL = "dataset_demo/train/ants/0013035.jpg"
+
+
+# writer.add_scalar()的使用
+def add_scalar():
+    writer = SummaryWriter("log_scalar")
+    for i in range(-50, 51):
+        writer.add_scalar("y=x^2", i * i, i)
         pass
     writer.close()
+    pass
+
+
+# writer.add_image()的使用
+def add_image():
+    writer = SummaryWriter("log_image")
+    img = Image.open(IMAGE_URL)
+    img_ndarray = numpy.array(img)  # HWC
+    writer.add_image("tensorboard_image", img_ndarray, 1, dataformats="HWC")
+    writer.close()
+    pass
+
+
+if __name__ == "__main__":
+    add_scalar()    # writer.add_scalar()的使用
+    add_image()     # writer.add_image()的使用
     pass
